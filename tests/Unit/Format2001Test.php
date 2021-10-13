25114de828c623536eb9a799cf5d1d7d5cb645d8
Diego Calle <dnetix@gmail.com>
2021-10-12 22:04:41 -0500

    Reformat tests

--- tests/Unit/Format2001Test.php
+++ tests/Unit/Format2001Test.php
@@ -1,6 +1,10 @@
 <?php
 
+namespace Tests\Unit;
+
 use Dnetix\Asoban\AsobanParser;
+use Dnetix\Asoban\Exceptions\AsobanException;
+use Tests\BaseTestCase;
 
 class Format2001Test extends BaseTestCase
 {
@@ -10,16 +14,14 @@ class Format2001Test extends BaseTestCase
 
         $asoban = AsobanParser::load($file, AsobanParser::F_2001)->parse();
 
-        $this->assertEquals('12345', $asoban->header()->nit());
+        $this->assertEquals('0000012345', $asoban->header()->nit());
         $this->assertEquals('00849514521', $asoban->header()->accountNumber());
-        $this->assertEquals('1', $asoban->header()->accountType());
+        $this->assertEquals('01', $asoban->header()->accountType());
     }
 
-    /**
-     * @expectedException \Exception
-     */
     public function testItThrowsExceptionWhenFileDoesntExists()
     {
+        $this->expectException(AsobanException::class);
         AsobanParser::load(__DIR__ . '/files/not-exists', AsobanParser::F_2001)->parse();
     }
 }
