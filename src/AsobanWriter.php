f6abad91c350d8099d861149e17cb8e2e5aa1628
Diego Calle <dnetix@gmail.com>
2021-10-27 15:36:06 -0500

    Add control line writer

--- src/AsobanWriter.php
+++ src/AsobanWriter.php
@@ -2,6 +2,7 @@
 
 namespace Dnetix\Asoban;
 
+use Dnetix\Asoban\Entities\AsobanControl;
 use Dnetix\Asoban\Entities\AsobanHandler;
 use Dnetix\Asoban\Parsers\Format2001;
 
@@ -30,6 +31,8 @@ class AsobanWriter extends AsobanHandler
 
         $this->writeLine($formatter->headerLine($this->header()));
 
+        $totalAmount = 0;
+        $totalCount = 0;
         foreach ($this->batchs as $batch) {
             $amount = 0;
             $count = 0;
@@ -39,8 +42,17 @@ class AsobanWriter extends AsobanHandler
                 $count++;
                 $this->writeLine($formatter->recordLine($record));
             }
+            $totalAmount += $amount;
+            $totalCount += $count;
             $this->writeLine($formatter->endBatchLine($batch->endBatch()));
         }
+
+        $this->writeLine($formatter->controlLine(
+            new AsobanControl([
+                'records' => $totalCount,
+                'amount' => $totalAmount,
+            ])
+        ));
 
         fclose($this->file);
     }
